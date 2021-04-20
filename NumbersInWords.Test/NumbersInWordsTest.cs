47be2883ed45a202ebb4207135c7d876b78f00c3
Vincent Dittli <vincent.dittli@bbv.ch>
2021-04-20 13:13:42 +0200

    Implemented single digit numbers

--- NumbersInWords.Test/NumbersInWordsTest.cs
+++ NumbersInWords.Test/NumbersInWordsTest.cs
@@ -5,30 +5,27 @@ namespace NumbersInWords.Test
 
     public class NumbersInWordsTest
     {
-        [Fact]
-        public void Number1ShouldBe_One()
+        [Theory]
+        [InlineData(0,"Zero")]
+        [InlineData(1,"One")]
+        [InlineData(2,"Two")]
+        [InlineData(3,"Three")]
+        [InlineData(4,"Four")]
+        [InlineData(5,"Five")]
+        [InlineData(6,"Six")]
+        [InlineData(7,"Seven")]
+        [InlineData(8,"Eight")]
+        [InlineData(9,"Nine")]
+        public void ShouldReturnCorrectString(int input, string expected)
         {
             // Arrange
             NumbersInWords testee = new NumbersInWords();
 
             // Act
-            string result = testee.Convert(1);
+            string result = testee.Convert(input);
 
             // Assert
-            result.Should().Be("One");
-        }
-
-        [Fact]
-        public void Number2ShouldBe_Two()
-        {
-            // Arrange
-            NumbersInWords testee = new NumbersInWords();
-
-            // Act
-            string result = testee.Convert(2);
-
-            // Assert
-            result.Should().Be("Two");
+            result.Should().Be(expected);
         }
     }
 }
--- NumbersInWords/NumbersInWords.cs
+++ NumbersInWords/NumbersInWords.cs
@@ -4,11 +4,19 @@
     {
         public string Convert(int number)
         {
-            if (number == 2)
+            switch (number)
             {
-                return "Two";
+                case 9: return "Nine";
+                case 8: return "Eight";
+                case 7: return "Seven";
+                case 6: return "Six";
+                case 5: return "Five";
+                case 4: return "Four";
+                case 3: return "Three";
+                case 2: return "Two";
+                case 1: return "One";
+                default: return "Zero";
             }
-            return "One";
         }
     }
 }
